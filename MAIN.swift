7bf0ff9042ce00b7b2ab475462e3ab0d0ac7d9fd
Zoe Cutler <36316793+zcutlerf@users.noreply.github.com>
2024-07-22 15:28:22 -0400

    Reverted main method

--- MAIN.swift
+++ MAIN.swift
@@ -7,31 +7,26 @@ extension CommandLineManager {
         // print - prints to console
         print("Enter your name:")
         
-//        // readString - user inputs string, then presses return
-//        let name = await readString()
-//        print("Your name is \(name)")
-//        
-//        // other input methods - returns data type if user inputs correctly, or nil if they don't
-//        let _ = await readInt()
-//        let _ = await readDouble()
-//        let _ = await readBool()
-//        
-//        // awaitKeyPress = awaits one or more keys, returns key that was pressed
-//        await awaitKeyPress(keys: [.leftArrow])
-//        print("You pressed left arrow")
-//        
-//        let key = await awaitKeyPress(keys: [.w, .a, .s, .d])
-//        switch key {
-//        case .w: print("w")
-//        case .a: print("a")
-//        case .s: print("a")
-//        case .d: print("d")
-//        default: break
-//        }
+        // readString - user inputs string, then presses return
+        let name = await readString()
+        print("Your name is \(name)")
         
-        while true {
-            let key = await awaitKeyPress(keys: Key.allCases)
-            print(key.rawValue)
+        // other input methods - returns data type if user inputs correctly, or nil if they don't
+        let _ = await readInt()
+        let _ = await readDouble()
+        let _ = await readBool()
+        
+        // awaitKeyPress = awaits one or more keys, returns key that was pressed
+        await awaitKeyPress(keys: [.leftArrow])
+        print("You pressed left arrow")
+        
+        let key = await awaitKeyPress(keys: [.w, .a, .s, .d])
+        switch key {
+        case .w: print("w")
+        case .a: print("a")
+        case .s: print("a")
+        case .d: print("d")
+        default: break
         }
     }
 }
