3318a9562388f3839ebeff0232afb932f5a0522f
Zoe Cutler <36316793+zcutlerf@users.noreply.github.com>
2024-07-23 11:13:55 -0400

    Added "none" option to key to enable timeout

--- CommandLineSwiftUI/Key.swift
+++ CommandLineSwiftUI/Key.swift
@@ -6,9 +6,12 @@ enum Key: String, CaseIterable {
     case a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
     case num1, num2, num3, num4, num5, num6, num7, num8, num9, num0
     case backtick = "`", hyphen = "-", equals = "=", squareBracketLeft = "[", squareBracketRight = "]", backslash = "\\", semicolon = ";", apostrophe = "'", comma = ",", period = ".", slash = "/"
+    case none = "No keys pressed"
     
     var keyEquivalent: KeyEquivalent {
         switch self {
+        case .none:
+                .end
         case .upArrow:
                 .upArrow
         case .downArrow:
